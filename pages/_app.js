12f9948efc420c36b0e69f38a6495b6c28f4ca39
Danilo Yorinori <daniloksy@gmail.com>
2021-08-30 20:55:39 -0300

    refactor(_app): remove providers and theme which was moved to Website Wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { PropTypes } from 'prop-types';
-import { ThemeProvider } from 'styled-components';
-import theme from '../src/theme';
-import GlobalStyle from '../src/theme/GlobalStyle';
+import PropTypes from 'prop-types';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   );
 }

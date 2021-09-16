372c8f428eb9480398c3f19334ee498bff910419
Danilo Yorinori <daniloksy@gmail.com>
2021-09-15 21:02:58 -0300

    fix(breakpointsmedia): change file location to ease unit tests and fix breakpoints handling

--- src/theme/utils/breakpointsMedia/index.js
+++ src/theme/utils/breakpointsMedia/index.js
@@ -1,10 +1,11 @@
 import { css } from 'styled-components';
-import { breakpoints } from '../index';
+import { breakpoints } from '../../index';
 
 export default function breakpointsMedia(cssByBreakpoints) {
   const breakpointsNames = Object.keys(cssByBreakpoints);
   return breakpointsNames
-    .filter((breakpointName) => Boolean(cssByBreakpoints[breakpointName]))
+    .filter((breakpointName) => Boolean(cssByBreakpoints[breakpointName])
+      && breakpoints[breakpointName] !== undefined)
     .map((breakpointName) => css`
         @media screen and (min-width : ${breakpoints[breakpointName]}px) {
           ${cssByBreakpoints[breakpointName]}

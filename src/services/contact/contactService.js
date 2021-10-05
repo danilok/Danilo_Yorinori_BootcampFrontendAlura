a663e80e9dc8d0727685a48a932873f4dc94bc38
Danilo Yorinori <daniloksy@gmail.com>
2021-10-05 20:06:16 -0300

    refactor(contact-service): change endpoint url based on isStagingEnv value

--- src/services/contact/contactService.js
+++ src/services/contact/contactService.js
@@ -1,11 +1,16 @@
 import HttpClientService from '../httpClient/httpClientService';
+import isStagingEnv from '../env/isStagingEnv';
+
+const BASE_URL = isStagingEnv
+  ? 'https://contact-form-api-jamstack.herokuapp.com/message'
+  : '/api/messages';
 
 const contactService = {
   async sendMessage(
     contactDTO,
     HttpClientModule = HttpClientService,
   ) {
-    const response = await HttpClientModule('/api/messages', {
+    const response = await HttpClientModule(BASE_URL, {
       method: 'POST',
       body: contactDTO,
     });
